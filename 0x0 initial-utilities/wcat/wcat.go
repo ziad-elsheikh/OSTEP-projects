cb081d0b40c35342c7c55eb68dfa78d4e4452aec
Ziad <ziad.elsheikh.dev@gmail.com>
2026-06-21 23:16:14 +0300

    implement cat in two difference way

--- 0x0 initial-utilities/wcat/wcat.go
+++ 0x0 initial-utilities/wcat/wcat.go
@@ -3,19 +3,44 @@ package main
 import (
 	"fmt"
 	"os"
+	"io"
 )
 
 func main(){
+	fmt.Println("method1")
+	// cat1()
+	cat2()
+}
+
+
+func cat1(){
+	for _, fname := range os.Args[1:]{
+		file, err := os.ReadFile(fname)
+		
+		if err != nil{
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		fmt.Println(string(file))
+	}
+}
+
+
+func cat2(){ // using OS && IO 
 	files := os.Args[1:]
 	
-	for _, file := range files {
-		data, err := os.ReadFile(file)
-	
-		if err != nil{
-			fmt.Printf("%e", err)
-		}
+		for _, file := range files { // looping on all files
+			data, err := os.Open(file) // open and read in once
+		
+			if err != nil{
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
 
-		fmt.Printf("%v", string(data))
+			if _, err := io.Copy(os.Stdout, data); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			data.Close()
 	}
-
 }
